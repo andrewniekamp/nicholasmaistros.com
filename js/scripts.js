460038fd0fa673f3d3e2324b73617fdee81784f2
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-09-18 11:07:06 -0700

    add fixed class to quote box when scrolled past

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,10 @@
-// getScores(self): any {
-//       var theUrl = "https://nicholasmaistros-779db.firebaseio.com/
-// e.json";
-//       var theData;
-//
-//       $.ajax({
-//         type: "GET",
-//         headers: {
-//           //API key from Firebase can go here...
-//           'Accept': 'application/json',
-//           'Content-Type': 'application/json'
-//         },
-//         dataType: "json",
-//         url: theUrl,
-//         data: JSON.stringify(theData),
-//         success: function (data) {
-//           //Have to push data from Firebase to array to deal with Angular2
-//           self.pushDataToArray(data);
-//         }
-//       });
+$(window).scroll(function() {
+    var fromTop = $(this).scrollTop();
+    if (fromTop >= $('#banner-box').height()) {
+        $('#quote-box').addClass('fixed');
+        console.log("added");
+    } else {
+        $('#quote-box').removeClass('fixed');
+        console.log("removed");
+    }
+});
