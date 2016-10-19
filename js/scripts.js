5f43cba68b2f0b88caea59bb8f161aaf9c65ff16
Andrew Niekamp <andrew.f.niekamp@gmail.com>
2016-10-18 21:59:53 -0700

    add header and nav redesign and update some content

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 var quotes = [
   "<p>They brushed the snow from their eyelashes, and they looked out across the divide. It was hard to picture a steam engine here, grinding and huffing behind them, but the Major's notebooks were filling with inky measurements. He could see it. And what a view.</p><p>-These Imaginary Acts</p>",
   "<p>A real thwack, on that rubbery dead cheek. And she must have thought that would bring his awful ugly soul back into him, because she ran, out of the room and right into me.</p><p>-Whichever story</p>",
-  "<p>Say I <em>liked</em> it. Say I <em>loved</em> it. Say <em>Thank you for sharing this with me</em>. No, don't say any of that. I'll have no idea if it's true. Say something else.</p><p>-These Imaginary Acts</p>",
   "<p>And you miss all the ways you could have been admired and loved and hated by lovers and enemies and coworkers in cities like New York, or better than New York, cities that don't even exist.</p><p>-The Brights</p>",
   "<p>He was firm in who he was and had been, a certainty, not because he felt it was right, nothing so patriotic-American, but because the wrong things never happened to him. </p><p>-All the Crowned Ruffians That Ever Lived</p>",
   "<p>Missy was in this bathroom on a yacht. Her father was recovering in someone's island mansion. Manny was shooting at the Taliban. No one was where they were supposed to be.</p><p>-All the Crowned Ruffians That Ever Lived</p>",
@@ -14,18 +13,18 @@ $(window).scroll(function() {
     var fromTop = $(this).scrollTop();
     if (fromTop >= $('#banner-box').height()) {
         $('#sticky-section').addClass('fixed');
-        $('#quote-box').addClass('smaller');
         $('.main-container').addClass('padded-container');
+        $('.anchor').removeClass('nav-padding');
     } else {
         $('#sticky-section').removeClass('fixed');
-        $('#quote-box').removeClass('smaller');
         $('.main-container').removeClass('padded-container');
+        $('.anchor').addClass('nav-padding');
     }
 });
 
 var scrollAnimation = function(destination) {
   $('html, body').animate({
-    scrollTop: $(destination).offset().top-120
+    scrollTop: $(destination).offset().top+50
   }, 1000);
 };
 
@@ -45,7 +44,11 @@ $(document).ready(function() {
   setInterval(
       function rotateQuote() {
         var num = Math.floor(Math.random() * (quotes.length));
-        $(".quote-fader").html(quotes[num]);
-        console.log(num);
-      }, 15000);
+        $(".quote-fader").fadeOut(function() {
+          $(".quote-fader").html(quotes[num]);
+        });
+        $(".quote-fader").fadeIn(function() {
+          $(".quote-fader").html(quotes[num]);
+        });
+      }, 5000);
 });
